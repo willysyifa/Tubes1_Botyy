e0089171ec0fda5a0aacc5f49deb65e397b403e0
Willy Syifa Luthfia <willy.123140071@student.itera.ac.id>
2025-06-01 21:47:48 +0700

    Add files via upload

---
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
@@ -0,0 +1,4 @@
+@echo off
+start cmd /c "python main.py --logic greedy_distance --email=nbla@email.com --name=nbla --password=123456 --team etimo"
+start cmd /c "python main.py --logic greedy_value --email=wily@email.com --name=wily --password=123456 --team etimo"
+start cmd /c "python main.py --logic greedy_value_distance --email=jska@email.com --name=jska --password=123456 --team etimo"
